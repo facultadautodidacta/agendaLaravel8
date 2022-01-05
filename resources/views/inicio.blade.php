f6840b131799ff3a8d109b88685aae5e77ca0478
Roldan Aquino <roldanaquinosegura@gmail.com>
2022-01-04 22:40:45 -0600

    agregado de menu responsive design

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -3,5 +3,25 @@
 @section('tituloPagina', 'Inicio')
 
 @section('contenido')
-    
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <h2>Esta es una agenda sencilla con laravel 8</h2>
+                        </div>
+                    </div>
+                    <div class="row text-center">
+                        <div class="col-sm-12">
+                            <img class="img-thumbnail" src="{{ asset('img/foto.png') }}" alt="" width="30%" >
+                            <p style="font-family: 'The Nautigal', cursive;">
+                                Este es mi primer agenda con laravel 
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/layouts/agenda.blade.php
+++ resources/views/layouts/agenda.blade.php
@@ -11,10 +11,46 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    
     <title>@yield('tituloPagina')</title>
   </head>
   <body>
-    @yield('contenido')
+
+    <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="{{ route('inicio') }}">
+            <span class="fas fa-address-card fa-2x"></span>
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="{{ route('inicio') }}">
+                  <span class="fas fa-home"></span>  Inicio
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span class="fas fa-list-alt"></span> Categorias
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                <span class="fas fa-id-card"></span> Contactos
+                </a>
+              </li>
+            </ul>
+           
+          </div>
+        </div>
+      </nav>
+      @yield('contenido')
+    </div>
+
+    
 
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
